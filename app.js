591b70d4ab8b347597a450d446d19469f2f21898
mihaelagafitescu <mihaela.gafitescu@gmail.com>
2015-01-28 01:11:17 +0200

    - changed url to server - suggestions

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 (function () {
-    var app = angular.module('winesite', ["ui.bootstrap.modal"]);
+    var shufflemodule = angular.module('ShuffleModule', []);
+
+    shufflemodule.filter('shuffle', function() {
+        var shuffledArr = [],
+            shuffledLength = 0;
+        return function(arr) {
+            var o = arr.slice(0, arr.length);
+            if (shuffledLength == arr.length) return shuffledArr;
+            for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            shuffledArr = o;
+            shuffledLength = o.length;
+            return o;
+        };
+    });
+
+
+    var app = angular.module('winesite', ["ui.bootstrap.modal", "ShuffleModule"]);
 
 
     app.controller('SiteController', function ($http, $scope) {
         var site = this;
         site.tab = 1;
-        site.showDetails =  false;
+        site.showDetails = false;
         site.wines = [];
+        site.redWineSuggestions = ['aa', 'bb', 'cc', 'dd'];
 
 
         //for search from the topbar with keyword
-        site.search = function (){
-            alert(site.keyword);
+        site.search = function () {
+            //alert(site.keyword);
+            var url = '';
+            url = url + '/?search=' + site.keyword;
+            alert(url);
             $http.get('freebaseK.json').success(function (data) {
                 var results = data;
                 site.wines = results;
@@ -23,8 +43,19 @@
         };
 
         //for search from the Find Info section with constraints
-        site.findInfo = function (){
-            alert(site.find.name + " " + site.find.vintage + " " + site.find.color);
+        site.findInfo = function () {
+            var url = '';
+            url = url + '/?name=' + site.find.name +
+                    '&country=' + site.find.country +
+                    '&region=' + site.find.region +
+                    '&wine_producer=' + site.find.wine_producer +
+                    '&vineyard=' + site.find.vineyard +
+                    '&vintage=' + site.find.vintage +
+                    '&percentage_alcohol=' + site.find.percentage_alcohol +
+                    '&color=' + site.find.color +
+                    '&wine_type=' + site.find.wine_type +
+                    '&fruit_source=' + site.find.fruit_source;
+            //alert(site.find.name + " " + site.find.vintage + " " + site.find.color);
             $http.get('freebase.json').success(function (data) {
                 var results = data;
                 site.wines = results;
@@ -35,9 +66,9 @@
             site.keyword = null;
         };
 
-        $scope.goToFindInfo = function(){
+        $scope.goToFindInfo = function () {
             site.setTab(2);
-        }
+        };
 
         //for tabs
         site.isSet = function (checkTab) {
@@ -49,16 +80,19 @@
         };
 
         //for details
-        $scope.open = function(wine) {
+        $scope.open = function (wine) {
             $scope.showModal = true;
             $scope.wine = wine;
         };
-     /*   $scope.ok = function() {
-            $scope.showModal = false;
-        };*/
-        $scope.cancel = function() {
+        /*   $scope.ok = function() {
+         $scope.showModal = false;
+         };*/
+        $scope.cancel = function () {
             $scope.showModal = false;
         };
+
+
+
     });
 
 
@@ -99,5 +133,8 @@
             templateUrl: "contact.html"
         };
     });
+
+
+
 
 })();
